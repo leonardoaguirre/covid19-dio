28c6a7542da4489566a766e343f9d8167355ef7b
leonardoaguirre <62442754+leonardoaguirre@users.noreply.github.com>
2022-03-31 14:48:43 -0300

    add board and panel to main

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,10 +1,13 @@
 import api from "api";
 import React, { memo, useEffect, useState } from "react";
+import Board from "./components/Board";
+import Panel from "./components/Panel";
 import { ContainerStyled } from "./style";
 
 function Main() {
     const [data, setData] = useState({})
     const [country, setCountry] = useState('brazil')
+    const updatedAt = new Date().toLocaleDateString()
 
     const getCovidData = (country) => {
         api.getCountry(country).then(data => setData(data))
@@ -14,11 +17,22 @@ function Main() {
         getCovidData(country)
     }, [country])
 
+    const handleChange = ({ target }) => {
+        const country = target.value
+        setCountry(country)
+    }
+
     return (
         <ContainerStyled>
             <div className="mb-2">
-
+                <Panel
+                    data={data}
+                    updateAt={updatedAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData} />
             </div>
+            <Board data={data} />
         </ContainerStyled>
     )
 }
